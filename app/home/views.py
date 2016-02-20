4ecb5e00fe4915bd9d8928bc263d2b33d0ddc5a0
pc-mist <mistsxd@gmail.com>
2016-02-20 23:09:17 +0700

    Fixed authorization

--- app/home/views.py
+++ app/home/views.py
@@ -22,7 +22,7 @@ class LoginFormView(FormView):
 class LogoutView(View):
     def get(self, request):
         logout(request)
-        return HttpResponseRedirect("/home")
+        return HttpResponseRedirect("/")
 
 
 class HomeView(View):
