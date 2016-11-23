56f7c298a76a9e8754d0e44bcd51a48a27fb2a78
pc-mist <mistsxd@gmail.com>
2016-11-23 21:13:18 +0700

    gg20311164815qq

--- app/library/urls.py
+++ app/library/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
+from .views import PePe
 
 urlpatterns = [
     url(r'^api/', include('app.library.api.urls')),
+    url(r'^main/$', PePe.as_view(), name='pepe')
 ]
